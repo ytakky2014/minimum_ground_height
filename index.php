b44ae2e18aa497c5340335dbe3952a242a81e060
ytakky2014 <ta_yoshida@ta-yoshida.local>
2016-10-04 22:25:50 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+$minimum_ground_heihgt = mt_rand(2,3) + mt_rand(0,10) * 0.1;
+echo "最低地上高は" . $minimum_ground_heihgt . "mm";
+?>
+</body>
+</html>
